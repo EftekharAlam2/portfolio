769be6ad65bc6b549fe52156c697091e4f100fd3
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-07-07 11:07:22 +0600

    about section edit

--- src/Home/About.jsx
+++ src/Home/About.jsx
@@ -1,35 +1,64 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
 const About = () => {
   return (
-    <section className="bg-gradient-to-b from-purple-500 to-indigo-600 py-16">
-      <div className="container mx-auto px-4">
-        <div className="max-w-3xl mx-auto text-center">
-          <h2 className="text-4xl font-bold text-white mb-8">About Me</h2>
-          <p className="text-xl text-white mb-8">
+    <div>
+      <div>
+        <h1 className="text-center my-5 mt-20 text-3xl font-semibold">
+          About Me
+        </h1>
+      </div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        >
+          <h3 className="vertical-timeline-element-title">
             I am deeply committed to self-improvement and continuous learning,
             especially since the beginning of this year when I started my
             journey to become a web developer. Every day, I dedicate myself to
             refining my skills and expanding my knowledge in technologies such
             as HTML, CSS, Bootstrap, Tailwind, Node.js, React.js, JavaScript,
             MongoDB, and Firebase.
-          </p>
-          <p className="text-xl text-white mb-8">
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(233, 30, 99)" }}
+          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+        >
+          <h3 className="vertical-timeline-element-title">
             Through my ongoing journey of self-directed learning, I have worked
             diligently to establish a solid foundation in both front-end and
             back-end development. Additionally, my involvement in volunteer
             activities has greatly enhanced my communication, teamwork, and
             interpersonal skills.
-          </p>
-          <p className="text-xl text-white">
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(16, 204, 82)" }}
+          iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+        >
+          <h3 className="vertical-timeline-element-title">
             I am proud to have achieved notable accomplishments, including
             participation in the Hult Prize Ideal Contest, the ICPC Programming
             Contest, and winning the Tech Debate Contest as part of a successful
             team. These experiences have not only allowed me to apply my
             technical knowledge but have also strengthened my critical thinking,
             problem-solving, and ability to perform under pressure.
-          </p>
-        </div>
-      </div>
-    </section>
+          </h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
   );
 };
 
