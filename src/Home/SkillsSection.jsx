f03458eb0eafaf8d732c352e326db56da68665f4
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-07-07 15:13:59 +0600

    animation added

--- src/Home/SkillsSection.jsx
+++ src/Home/SkillsSection.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
+import "./skills.css";
 const SkillsSection = () => {
+  const [hoveredSkill, setHoveredSkill] = useState(null);
+
+  const handleSkillHover = (skillName) => {
+    setHoveredSkill(skillName);
+  };
+
   return (
     <div id="skills" className="container mx-auto p-4">
       <h2 className="text-2xl font-bold mb-8 mt-4 text-center">My Skills</h2>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "HTML" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("HTML")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/km3vHBm/html5.png"
             alt="HTML Logo"
@@ -12,7 +27,13 @@ const SkillsSection = () => {
           />
           <span className="font-bold">HTML</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "CSS" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("CSS")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/R4bV25W/css3.png"
             alt="CSS Logo"
@@ -20,7 +41,14 @@ const SkillsSection = () => {
           />
           <span className="font-bold">CSS</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Tailwind CSS" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Tailwind CSS")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/v36YY4p/tailwind.png"
             alt="Tailwind CSS Logo"
@@ -28,7 +56,13 @@ const SkillsSection = () => {
           />
           <span className="font-bold">Tailwind CSS</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Bootstrap" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Bootstrap")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/9rYcF4B/bootstrap.png"
             alt="Bootstrap Logo"
@@ -36,7 +70,13 @@ const SkillsSection = () => {
           />
           <span className="font-bold">Bootstrap</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "JavaScript" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("JavaScript")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/BTdwRZg/javascript.png"
             alt="JavaScript Logo"
@@ -44,7 +84,13 @@ const SkillsSection = () => {
           />
           <span className="font-bold">JavaScript</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "React" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("React")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/vZmxZxN/react.png"
             alt="React Logo"
@@ -52,7 +98,13 @@ const SkillsSection = () => {
           />
           <span className="font-bold">React</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "Firebase" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("Firebase")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/Z6VddKK/firebase.png"
             alt="Firebase Logo"
@@ -60,7 +112,13 @@ const SkillsSection = () => {
           />
           <span className="font-bold">Firebase</span>
         </div>
-        <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-center">
+        <div
+          className={`bg-white shadow-lg rounded-lg p-4 flex items-center justify-center skill-card ${
+            hoveredSkill === "MongoDB" ? "hovered" : ""
+          }`}
+          onMouseEnter={() => handleSkillHover("MongoDB")}
+          onMouseLeave={() => handleSkillHover(null)}
+        >
           <img
             src="https://i.ibb.co/cbVTDQD/mongodb.png"
             alt="MongoDB Logo"
