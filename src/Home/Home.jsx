7d20e7904541c16c673951aa9bc3db79ba965288
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-07-21 08:28:22 +0600

    particles added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Particles from "react-tsparticles";
 import About from "./About";
 import Banner from "./Banner";
 import ContactUs from "./ContactUs";
@@ -5,10 +6,88 @@ import Footer from "./Footer";
 import NavBar from "./NavBar";
 import Project from "./Project";
 import SkillsSection from "./SkillsSection";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim";
 
 const Home = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   return (
     <div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 400,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 1,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.9,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#0d47a1",
+            },
+            links: {
+              color: "#0d47a1",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 12,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <NavBar></NavBar>
       <Banner></Banner>
       <About></About>
